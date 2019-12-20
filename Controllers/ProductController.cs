4bde1e0dc2a24418623d377778c5f164919e56ec
HossamGad <54635291+HossamGad@users.noreply.github.com>
2019-12-20 21:35:54 +0100

    Add files via upload

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Bageriet.Models;
+using Bageriet.ViewModels;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Bageriet.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductRepository _productRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public ProductController(IProductRepository productRepository, ICategoryRepository categoryRepository)
+        {
+            _productRepository = productRepository;
+            _categoryRepository = categoryRepository;
+        }
+
+        public ViewResult ProductList()
+        {
+            ProductListViewModel ProductListViewModel = new ProductListViewModel();
+            ProductListViewModel.Products = _productRepository.AllProducts;
+
+            return View(ProductListViewModel);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var product = _productRepository.GetProductById(id);
+            if (product == null)
+                return NotFound();
+            return View(product);
+        }
+    }
+}
